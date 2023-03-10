c3c28775d2385cc8e2d1c4add8612d04c1bfa9f5
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2023-03-10 16:51:02 -0500

    Added run command to test on remote PI

---
+++ run.sh
@@ -0,0 +1,3 @@
+scp acfd.py schieder@192.168.0.156:/home/schieder/Code/AutomatedCatFeedingDevice/acfd.py
+ssh -t schieder@192.168.0.156 'cd Code/AutomatedCatFeedingDevice; python3 acfd.py; bash -l'
+
